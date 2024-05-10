1cee5c7b3545d7862e4a35b40ea3c85c8274c71d
Alex Cheung <alex.cheung@insidedesk.com>
2024-05-09 23:21:38 -0700

    feat: added draw_grid_lines function

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ clock = pygame.time.Clock()
 running = True
 
 
-def main():
+def draw_grid_lines() -> None:
+    for i in range(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE):
+        pygame.draw.line(screen, config.GRID_COLOR, (i, 0), (i, config.SCREEN_SIZE))
+        pygame.draw.line(screen, config.GRID_COLOR, (0, i), (config.SCREEN_SIZE, i))
+
+
+def main() -> None:
     global running
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 running = False
 
-        screen.fill('orange')
+        screen.fill(config.BG_COLOR)
+        draw_grid_lines()
+
         pygame.display.flip()
         clock.tick(config.FPS)
     pygame.quit()
