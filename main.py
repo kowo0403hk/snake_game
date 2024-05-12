e74b904783d8736cb6de8211f28b679963a1bc0f
Alex Cheung <alex.cheung@insidedesk.com>
2024-05-11 20:40:02 -0700

    feat: added snake body (parts)

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
+from pygame import Vector2
 import config
 
 screen = pygame.display.set_mode((config.SCREEN_SIZE, config.SCREEN_SIZE))
 clock = pygame.time.Clock()
 running = True
+begin = True
+
+snake_rect = None
+snake_length = None
+snake_parts = None
+snake_direction = None
 
 
 def draw_grid_lines() -> None:
@@ -12,9 +19,14 @@ def draw_grid_lines() -> None:
         pygame.draw.line(screen, config.GRID_COLOR, (0, i), (config.SCREEN_SIZE, i))
 
 
-def main() -> None:
-    global running
+if __name__ == "__main__":
     while running:
+        if begin:
+            begin = False
+            snake_rect = pygame.rect.Rect([200, 200, config.SNAKE_PART_SIZE, config.SNAKE_PART_SIZE])
+            snake_length = 1
+            snake_parts = []
+            snake_direction = Vector2(config.SNAKE_MOVE_DISTANCE, 0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 running = False
@@ -22,10 +34,13 @@ def main() -> None:
         screen.fill(config.BG_COLOR)
         draw_grid_lines()
 
+        # start moving snake body
+        snake_rect.move_ip(snake_direction)
+        snake_parts.append(snake_rect.copy())
+        snake_parts = snake_parts[-snake_length:]
+
+        [pygame.draw.rect(screen, config.SNAKE_COLOR, snake_part) for snake_part in snake_parts]
+
         pygame.display.flip()
         clock.tick(config.FPS)
     pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
