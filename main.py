40273718199de2fa9d81d27161f2a6d7b7078229
Alex Cheung <alex.cheung@insidedesk.com>
2024-05-12 00:40:55 -0700

    fix: added keyboard movement capturing

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ running = True
 begin = True
 
 snake = None
+snake_parts = []
+move_direction = ""
 
 
 def draw_grid_lines() -> None:
@@ -17,8 +19,19 @@ def draw_grid_lines() -> None:
         pygame.draw.line(screen, config.GRID_COLOR, (0, i), (config.SCREEN_SIZE, i))
 
 
+def capture_key(key) -> str:
+    if key == pygame.K_UP:
+        return "UP"
+    if key == pygame.K_DOWN:
+        return "DOWN"
+    if key == pygame.K_LEFT:
+        return "LEFT"
+    if key == pygame.K_RIGHT:
+        return "RIGHT"
+    return ""
+
+
 if __name__ == "__main__":
-    snake_parts = []
     while running:
         if begin:
             begin = False
@@ -27,6 +40,8 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 running = False
+            if event.type == pygame.KEYDOWN:
+                move_direction = capture_key(event.key)
 
         time_now = pygame.time.get_ticks()
         screen.fill(config.BG_COLOR)
@@ -35,7 +50,7 @@ if __name__ == "__main__":
         # start moving snake body
         if time_now - time > config.DELAY:
             time = time_now
-            snake_parts = snake.move()
+            snake_parts = snake.move(move_direction)
         [pygame.draw.rect(screen, config.SNAKE_COLOR, snake_part) for snake_part in snake_parts]
 
         pygame.display.flip()
--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import Vector2
 import config
 from random import randrange
 
@@ -12,10 +13,17 @@ class Snake:
              config.SNAKE_PART_SIZE])
         self.snake_parts = []
         self.snake_length = len(self.snake_parts) or 1
-        self.move_direction = pygame.Vector2(0, 0)
+        self.move_direction = {
+            "UP": Vector2(0, -config.SNAKE_MOVE_DISTANCE),
+            "DOWN": Vector2(0, config.SNAKE_MOVE_DISTANCE),
+            "LEFT": Vector2(-config.SNAKE_MOVE_DISTANCE, 0),
+            "RIGHT": Vector2(config.SNAKE_MOVE_DISTANCE, 0)
+        }
 
-    def move(self):
-        self.snake_rect.move_ip(self.move_direction)
+    def move(self, key_direction):
+        valid_direction = self.move_direction.get(key_direction, None)
+        if valid_direction:
+            self.snake_rect.move_ip(valid_direction)
         self.snake_parts.append(self.snake_rect.copy())
         self.snake_parts = self.snake_parts[-self.snake_length:]
         return self.snake_parts
